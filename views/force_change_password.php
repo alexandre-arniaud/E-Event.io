945293f486ef756a953b9769becca3042a4f5d87
Losofy <alexandre.arniaud@etu.univ-amu.fr>
2022-01-14 23:56:27 +0100

    Rework CSS force_change_pass page

--- views/force_change_password.php
+++ views/force_change_password.php
@@ -4,7 +4,7 @@ start_page('E-event.io | Modifiez votre mot de passe');
 ?>
 
     <form class="signup" action="/index.php" method="post">
-        <span class="signup-title">Dis donc ! C'est la première fois que vous vous connectez ?</span>
+        <span class="force-title">Dis donc ! C'est la première fois que vous vous connectez ?</span>
         <p>Par sécurité, modifiez votre mot de passe dès maintenant pour avoir accès pleinement au site</p>
 
         <div class="form-group">
